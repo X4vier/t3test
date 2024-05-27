de6affdc29e3ef3417497878173e3d7eec11310c
Xavier O'Rourke <xav@canva.com>
2024-05-28 05:58:45 +1000

    Linked db to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,6 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = duplicateList(
-  [
-    "https://utfs.io/f/d9e3d3cf-0547-4539-a814-3b80dcce4d39-1nviw6.png",
-    "https://utfs.io/f/c31d28ec-7ff0-4011-a26f-c2bf124df24e-vl1num.png",
-    "https://utfs.io/f/3fec323c-9912-44de-91c5-1f0258068a76-1k7wxn.png",
-  ],
-  5,
-);
-
 function duplicateList<T>(list: T[], times: number): T[] {
   const l: T[] = [];
   for (let i = 0; i < times; i++) {
@@ -20,22 +11,19 @@ function duplicateList<T>(list: T[], times: number): T[] {
   return l;
 }
 
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = duplicateList(
+    await db.query.images.findMany({
+      orderBy: (model, { desc }) => desc(model.id),
+    }),
+    4,
+  );
 
-  console.log(posts);
+  console.log(images);
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {mockImages.map((image, index) => (
+        {images.map((image, index) => (
           <div key={index} className="w-48 p-4">
             <img src={image.url} />
           </div>
